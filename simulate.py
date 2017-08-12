577bc5382368561bc8427831f2afbbfb78d0b2ab
unknown <hisume@outlook.com>
2017-08-11 21:12:12 -0400

    started simulate.py

---
+++ simulate.py
@@ -0,0 +1,17 @@
+import datetime
+
+
+class simulate():
+
+    def __init_(self, startAmount, data):
+        self.reserve=startAmount
+        if isinstance(a,list):
+            self.data=data
+        else:
+            raise ValueError("Data must be a list of time,value")
+        
+        self.frame=0
+
+
+        
+
